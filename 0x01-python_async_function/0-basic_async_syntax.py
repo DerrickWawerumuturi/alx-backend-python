8796b711d62d83515456866399201971228bbdaf
Derrick Muturi <wawerumuturi57@gmail.com>
2024-08-05 12:05:23 +0300

    async/await

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import asyncio as pause
+import random
+
+
+async def wait_random(max_delay: int = 10):
+    """ Waits for a random delay between 0 and the number given. """
+    delay = random.uniform(0, max_delay)
+    await pause.sleep(delay)
+    return delay
